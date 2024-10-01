2a92bdff1e565d8555b9130f8603352618dce299
Corbyn-daGama <corbyn.da-gama@mths.ca>
2024-10-01 11:09:07 -0400

    if button pressed A, it adds +1 number, if button press b = 0

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Corbyn da Gama
+ * Created on: Sep 2024
+ * This program Runs Cookie Clicker
 */
 
-basic.showString('Hello, World!')
+
+let cookieNumber: number 
+
+basic.clearScreen()
+
+cookieNumber = 0
+basic.showString(cookieNumber.toString())
+
+input.onButtonPressed(Button.A, function(){
+cookieNumber = cookieNumber + 1
+basic.showString(cookieNumber.toString())
+
+
+input.onButtonPressed(Button.B, function(){
+cookieNumber = cookieNumber = 0 
+basic.showString(cookieNumber.toString())})})
